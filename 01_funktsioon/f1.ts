659f0409353f4e4d823f6dd07d14e809f15e608a
Karl Otepalu <93727374+KOtepalu@users.noreply.github.com>
2023-02-06 16:26:57 +0200

    Add files via upload

---
+++ 01_funktsioon/f1.ts
@@ -0,0 +1,11 @@
+function korrutaKahega(arv: number): number{
+	return arv*2;
+}
+
+console.log(korrutaKahega(7));
+
+function keskmine(arv: number, arv2: number): number{
+	return (arv+arv2)/2;
+}
+
+console.log(keskmine(3,4));
