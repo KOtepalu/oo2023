bd74b8a88ade387500f6fc08e20c18853185c09e
KOtepalu <k.otepalu@gmail.com>
2023-02-17 15:38:09 +0200

    2 tund läbi

---
+++ 02_klass/Klass/src/Tellimus.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+import java.util.List;
+public class Tellimus {
+    Date tegemisAeg;
+    String tellija;
+    List<String> tooted;
+    boolean makstud;
+    public Tellimus(String tellija, List<String> tooted) {
+        this.tegemisAeg = new Date();
+        this.tellija = tellija;
+        this.tooted = tooted;
+        this.makstud = false;
+    }
+
+    public void muudaMakstuks(){
+        this.makstud = true;
+    }
+
+    public String KasOnMakstud(){
+        if (makstud) {
+            return "Makstud!";
+        } else {
+            return "Maksmata!";
+        }
+    }
+}
