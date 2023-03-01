5d46baffeeb1e8eb7d0705842f6ac49b0bb57bad
KOtepalu <k.otepalu@gmail.com>
2023-03-01 16:03:15 +0200

    Klass kodus

---
+++ 02_klass/Klass_kodus/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Klass broneering1 = new Klass("Karl","12345678","12.03.23","19:30");
+        Klass broneering2 = new Klass("Karl2","87654321","13.03.23","20:00");
+
+        String sonum = broneering1.loobuBroneeringust();
+        System.out.println(sonum);
+
+        String sonum2 = broneering2.muudaBroneeringut("Karl3","21436587","10.04.23", "18:30");
+        System.out.println(sonum2);
+    }
+    //KODUS: 1 klass, constructor, 2+ muutujat, 2+ funktsiooni, 2+ väljakutset mainis.
+}
